624d618c704b354e4c4d38119b9ebc72e792bc6f
zsq1234 <41423998+zsq1234@users.noreply.github.com>
2018-11-20 17:45:02 +0800

    Update get_seg_data.py

--- script/pre_deal/get_seg_data.py
+++ script/pre_deal/get_seg_data.py
@@ -4,8 +4,9 @@ import os.path
 import sys, time
 import os
 import codecs
+import json
 
-def get_seg_data(input_file, output_file):
+def get_seg_data(input_file, output_file, doPosTagging=False):
     """用海量分词对语料进行分词处理，获取分词的数据"""
 
     # jar包的路径
@@ -29,7 +30,7 @@ def get_seg_data(input_file, output_file):
 
 	#创建分词对象
     segmentor = BasicSegmentor()
-	
+
     dict_path = os.path.join(hl_path, 'dictionary/CoreDict.dat')
 	#加载词典, 如果没有用户自定义词典，可以用下面的语句加载，自定义词典需要注意文件码制
 	# if not segmentor.loadDictionary("../dictionary/CoreDict.dat", "../dictionary/userDict_utf8.txt"):
@@ -40,39 +41,40 @@ def get_seg_data(input_file, output_file):
     #创建SegOption对象，如果使用默认的分词选项，也可以直接传空
     option = SegOption()
     option.mergeNumeralAndQuantity = False
+    option.mergeNumeralAndQuantity = False  # 是否合并数词和量词
+    option.doPosTagging = doPosTagging  # 是否进行词性标注，默认False
 	#可以使用下面的语句调整分词颗粒度
     # option.grainSize = SegGrain.LARGE
+
+    if doPosTagging:
+        nature_dict_path = os.path.join(hl_path, 'dictionary/id2nature.json')
+        with open(, 'r', encoding='utf-8') as f:
+            id2nature_dict = json.load(f)
 
     # 打开文件
     try:
         input_data = codecs.open(input_file, 'r', 'utf-8')
         output_data = codecs.open(output_file, 'w', 'utf-8')
-
         for line in input_data.readlines():
-            # 分词
-            segResult = segmentor.segment(line, option)
+            segResult = segmentor.segment(line, option) # 分词
 
             # 遍历存储分词结果
             word = segResult.getFirst()
-            print('\n')
             while(word != None):
-                output_data.write(word.wordStr + ' ')
+                tmp = word.wordStr
+                if doPosTagging:
+                    tmp += id2nature_dict[str(word.nature)]
+                output_data.write(tmp + ' ')
                 word = word.next
             output_data.write('\n')
-        
+
         input_data.close()
         output_data.close()
-
     except:
         print('程序运行错误')
     finally:
         jpype.shutdownJVM()
 
 if __name__ == '__main__':
-    if len(sys.argv) == 3:
-        input_file = sys.argv[1]
-        output_file = sys.argv[2]
-    else:
-        input_file = '../../data/tmp/007001-010000_sentence_data.txt'
-        output_file = '../../data/tmp/007001-010000_seg_data.txt'
-    get_seg_data(input_file, output_file)
+    get_seg_data('../../data/tmp/000001-007000.txt', 
+                 '../../data/tmp/000001-007000_seg_data.txt')
